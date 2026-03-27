1558eabfd5de0dab0d6a0d91e5993c5b22864399
Sheena Ocon <oconsheen@gmail.com>
2026-03-27 20:36:22 +0800

    all thanks to mr bhardwaj and mr culbreth

--- js/constants.js
+++ js/constants.js
@@ -7,59 +7,30 @@ export const STAGGER_DELAY = 25;
 export const TOTAL_TRANSITION = 3800;
 export const MESSAGE_INTERVAL = 4000;
 
-export const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-!?\'/: ';
+export const CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-!?'/: ";
 
 export const SCRAMBLE_COLORS = [
-  '#00AAFF', '#00FFCC', '#AA00FF',
-  '#FF2D00', '#FFCC00', '#FFFFFF'
+  "#00AAFF",
+  "#00FFCC",
+  "#AA00FF",
+  "#FF2D00",
+  "#FFCC00",
+  "#FFFFFF",
 ];
 
 export const ACCENT_COLORS = [
-  '#00FF7F', '#FF4D00', '#AA00FF',
-  '#00AAFF', '#00FFCC'
+  "#00FF7F",
+  "#FF4D00",
+  "#AA00FF",
+  "#00AAFF",
+  "#0e6150",
 ];
 
 export const MESSAGES = [
-  [
-    '',
-    'GOD IS IN',
-    'THE DETAILS .',
-    '- LUDWIG MIES',
-    ''
-  ],
-  [
-    '',
-    'STAY HUNGRY',
-    'STAY FOOLISH',
-    '- STEVE JOBS',
-    ''
-  ],
-  [
-    '',
-    'GOOD DESIGN IS',
-    'GOOD BUSINESS',
-    '- THOMAS WATSON',
-    ''
-  ],
-  [
-    '',
-    'LESS IS MORE',
-    '',
-    '- MIES VAN DER ROHE',
-    ''
-  ],
-  [
-    '',
-    'MAKE IT SIMPLE',
-    'BUT SIGNIFICANT',
-    '- DON DRAPER',
-    ''
-  ],
-  [
-    '',
-    'HAVE NO FEAR OF',
-    'PERFECTION',
-    '- SALVADOR DALI',
-    ''
-  ]
+  ["", "PIG", "YOU ARE A PIG", "- PIG", ""],
+  ["", "YOU LOOK LIKE", "AN AI FRUIT", "- FRUIT AI", ""],
+  ["", "PIG DETECTED", "ITS YOU", "- PIG SYSTEM", ""],
+  ["", "YOU ARE NOT REAL", "YOU ARE AI FRUIT", "- GENERATED", ""],
+  ["", "PIG ENERGY", "STRONG TODAY", "- OINK", ""],
+  ["", "CONFIRMED", "100% PIG", "- VERIFIED", ""],
 ];
